de5e7ac21717f28d77bb2aef32afbf1dbf07d3ea
MariBayd <masha.baydina@gmail.com>
2021-01-24 14:17:14 +0300

    add script for adding new book

--- src/script.js
+++ src/script.js
@@ -3,11 +3,32 @@ var newItemForm = document.querySelector('.new-item-form'); //form of adding new
 
 var closeFormBtn = document.getElementById('closeBtn'); //btn for close form
 
+var submitBtn = document.getElementById('submit');
+
+var titleBook = document.getElementById('title');
+var authorBook = document.getElementById('author');
+var yearBook = document.getElementById('year');
+var genreBook = document.getElementById('genre');
+
+//create new object book
+function NewBook(title, author, year, genre) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.genre = genre;
+}
+
 //appearance of the form when adding a new book
 newBook.onclick = function() {
     newItemForm.style.display = "flex";
 }
 
+//close form for adding new book
 closeFormBtn.onclick = function() {
     newItemForm.style.display = "none";
+}
+
+//create new book 
+submitBtn.onclick = function() {
+    let newBook = new NewBook(titleBook, authorBook, yearBook, genreBook);
 }
