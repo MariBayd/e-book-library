50793e410a2e608e867dd8a87a37f22387bfb4c8
MariBayd <masha.baydina@gmail.com>
2021-01-24 03:14:04 +0300

    add close form

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
-var newBook = document.getElementById('newBook');
-var newItemForm = document.querySelector('.new-item-form');
+var newBook = document.getElementById('newBook'); //btn for add new book
+var newItemForm = document.querySelector('.new-item-form'); //form of adding new book
+
+var closeFormBtn = document.getElementById('closeBtn'); //btn for close form
 
 //appearance of the form when adding a new book
 newBook.onclick = function() {
     newItemForm.style.display = "flex";
+}
+
+closeFormBtn.onclick = function() {
+    newItemForm.style.display = "none";
 }
